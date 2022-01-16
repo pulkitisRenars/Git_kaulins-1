6d71549fbf006b2033d9df107568fe7ec044fdfa
Bugsters104 <r.kristaps@inbox.lv>
2022-01-16 21:38:43 +0200

    Papildināts ar funkcionalitāti, ka metamo kauliņu var mest vairākas reizes

--- src/Kaulins.java
+++ src/Kaulins.java
@@ -1,14 +1,16 @@
 import java.util.Random;
 
 public class Kaulins {
-	static void mestKaulinu() {
+	static void mestKaulinu(int reizes) {
 		int skaitlis;
 		Random rand = new Random();
-		skaitlis = rand.nextInt(6)+1;	
-		System.out.println("Uzkrita skaitlis "+skaitlis);
+		for(int i=1; i<=reizes; i++) {
+			skaitlis = rand.nextInt(6)+1;	
+			System.out.println("Uzkrita skaitlis "+skaitlis);
+		}
 	}
 	
 	public static void main(String[] args) {
-		mestKaulinu();
+		mestKaulinu(3);
 	}
 }
