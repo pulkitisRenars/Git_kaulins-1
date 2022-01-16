422296725e4893a452121eab5275ed67b90307c3
Bugsters104 <r.kristaps@inbox.lv>
2022-01-16 21:13:00 +0200

    Izstrādāta metamā kauliņa programmas sākotnējā versija

--- src/Kaulins.java
+++ src/Kaulins.java
@@ -1,9 +1,10 @@
+import java.util.Random;
 
 public class Kaulins {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int skaitlis;
+		Random rand = new Random();
+		skaitlis = rand.nextInt(6)+1;	
+		System.out.println("Uzkrita skaitlis "+skaitlis);
 	}
-
 }
