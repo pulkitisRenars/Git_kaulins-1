98279ffdb04f5ac413f1fe4b49538b414fe9c8a8
Bugsters104 <r.kristaps@inbox.lv>
2022-01-16 21:35:31 +0200

    Refaktorēts kods un ieviesta metode

--- src/Kaulins.java
+++ src/Kaulins.java
@@ -1,10 +1,14 @@
 import java.util.Random;
 
 public class Kaulins {
-	public static void main(String[] args) {
+	static void mestKaulinu() {
 		int skaitlis;
 		Random rand = new Random();
 		skaitlis = rand.nextInt(6)+1;	
 		System.out.println("Uzkrita skaitlis "+skaitlis);
+	}
+	
+	public static void main(String[] args) {
+		mestKaulinu();
 	}
 }
