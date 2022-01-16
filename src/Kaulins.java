483b2b45c39a49cb5f13cc60c20d2982f8edafab
Bugsters104 <r.kristaps@inbox.lv>
2022-01-16 21:46:11 +0200

    Iespējams ievadīt metienu skaitu no konsoles

--- src/Kaulins.java
+++ src/Kaulins.java
@@ -1,10 +1,17 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class Kaulins {
 	public static void main(String[] args) {
-		int skaitlis;
+		int skaitlis, reizes;
 		Random rand = new Random();
-		skaitlis = rand.nextInt(6)+1;	
-		System.out.println("Uzkrita skaitlis "+skaitlis);
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Cik reizes mest kauliņu?");
+		reizes = scan.nextInt();
+		for(int i=1; i<=reizes; i++) {
+			skaitlis = rand.nextInt(6)+1;	
+			System.out.println("Uzkrita skaitlis "+skaitlis);
+		}
+		scan.close();
 	}
 }
