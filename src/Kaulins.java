9def7f11813c2ec47b500d339f2d82f33d6a858c
Pulkis <renars.pulkis@gmail.com>
2022-02-01 11:34:49 +0200

    lai varētu pull request

--- src/Kaulins.java
+++ src/Kaulins.java
@@ -14,11 +14,25 @@ public class Kaulins {
 	public static void main(String[] args) {
 		int reizes;
 		Scanner scan = new Scanner(System.in);
+		int izvele;
 		do{
-		System.out.println("Cik reizes mest kauliņu?");
+			System.out.println("1-mest kaulinu | 2- apturet");
+			izvele = scan.nextInt();
+			switch(izvele){
+			case 1: 
+			
+		do{
+		System.out.println("Cik reizes mest kauli�u?");
 		reizes = scan.nextInt();
 		}while(reizes<1);
 		mestKaulinu(reizes);
 		scan.close();
+		break;
+		case 2: 
+			System.out.println("Programma aptureta");
+			break; 
+			default: System.out.println("Nav tadas opcijas, ja nu kas markuss small brein");
+		}
+		}while(izvele!=2);
 	}
 }
